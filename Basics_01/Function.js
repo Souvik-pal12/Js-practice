d0c9f991ca5f22703905f955db21d3cc1f9ce159
Jayanta Kumar <kumarjayanta9635@gmail.com>
2025-06-16 18:51:38 +0000

    new file added

---
+++ Basics_01/Function.js
@@ -0,0 +1,13 @@
+// This is all about Function
+/* Faunction name = maketea thart takes one parameter named ='typeOfTea',
+and returns a string like 'making green tea' when called with "green tea "
+ Store the result in a variable named 'teaOrder' */
+
+function maketea( typeOfTea) {
+   
+    return 'Making '+ typeOfTea;
+
+    console.log(test);
+}
+let teaOrder = maketea("green Tea");
+console.log(teaOrder);
